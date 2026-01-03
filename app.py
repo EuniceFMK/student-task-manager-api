0a5350935dd326531561be2f523b87e101e22a49
EuniceFMK <eunicengadjou2204@gmail.com>
2026-01-03 14:02:34 -0700

    Add basic Flask API for task management
    Introduces a Flask application with endpoints to get and add tasks. The API supports GET and POST methods at /tasks, allowing retrieval of all tasks and creation of new tasks with a title.

---
+++ app.py
@@ -0,0 +1,59 @@
+# def add_task(title):
+#     return {"title":title,"done":False}
+
+# tasks = []
+# tasks.append ("Finish assignment")
+
+# task = {
+#     "id":1,
+#     "title":"Study Python",
+#     "done":False
+# }
+
+# for task in tasks:
+#     print(task)
+
+from flask import Flask, jsonify, request    #Flask -- Class to create a web app/ jsonify is used for my app to speak web language
+ 
+app = Flask (__name__) #Create my flask application / app= my web server
+
+#List of tasks / Each task is a dictionnary (object)
+tasks = [
+    {"id":1,"title":"Study Python", "done": False},
+    {"id": 2, "title": "Build Flask API", "done": False}
+]
+
+# GET = Read Data / @app.route relate a URL with a Python function
+@app.route("/tasks", methods=["GET"])    # This line transform the python script in API REST
+def get_tasks():  #backend function
+    return jsonify(tasks)      #transform the python list in JSON
+
+  
+
+@app.route("/tasks",methods=["POST"])
+def add_task():
+    data=request.get_json()
+    
+    new_task ={
+        "id" :len(tasks)+1,
+        "title": data.get("title"),
+        "done":False
+    }
+
+    tasks.append(new_task)
+    return jsonify(new_task),201
+
+if __name__ == "__main__":   #Does the file executed directly?
+    app.run(debug=True)   
+
+# fetch("http://127.0.0.1:5000/tasks", {
+#   method: "POST",
+#   headers: {
+#     "Content-Type": "application/json"
+#   },
+#   body: JSON.stringify({
+#     title: "Prepare IBM application"
+#   })
+# })
+# .then(response => response.json())
+# .then(data => console.log(data));
